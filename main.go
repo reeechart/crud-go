92813eddf18bab0492f2d4885df9189bfe4f5a75
PT GO-JEK INDONESIA <ptgo-jekindonesia@PTs-MacBook-Air-2.local>
2018-07-05 16:28:31 +0700

    add read food test and implementation

--- main.go
+++ main.go
@@ -1,8 +1,55 @@
 package main
 
-import ("fmt")
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
 
-func main(){
-	fmt.Println("hello world")
+type Food struct {
+	id    int
+	name  string
+	price int
+	owner string
 }
 
+const (
+	DB_USERNAME = "postgres"
+	DB_PASSWORD = "postgres"
+	DB_NAME     = "food_test"
+)
+
+var (
+	db  *sql.DB
+	err error
+)
+
+func main() {
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USERNAME, DB_PASSWORD, DB_NAME)
+	db, err = sql.Open("postgres", dbinfo)
+	checkError(err)
+	defer db.Close()
+}
+
+func readFood(id int) *Food {
+	row := db.QueryRow("SELECT * FROM food WHERE ID=$1", id)
+
+	var foodId int
+	var name string
+	var price int
+	var owner string
+
+	err = row.Scan(&foodId, &name, &price, &owner)
+	if err == sql.ErrNoRows {
+		return nil
+	} else {
+		checkError(err)
+		return &Food{foodId, name, price, owner}
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ main_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USERNAME, DB_PASSWORD, DB_NAME)
+	db, err = sql.Open("postgres", dbinfo)
+	checkError(err)
+	defer db.Close()
+
+	testResult := m.Run()
+
+	os.Exit(testResult)
+}
+
+func TestReadFood(t *testing.T) {
+	food := readFood(1)
+	if food == nil {
+		t.Errorf("Food 1 is nil")
+	}
+	if food.id != 1 {
+		t.Errorf("Food ID is incorrect")
+	}
+	food = readFood(3)
+	if food == nil {
+		t.Errorf("Food 3 is nil")
+	}
+	if food.id != 3 {
+		t.Errorf("Food 3 ID is incorrect")
+	}
+	food = readFood(5)
+	if food != nil {
+		t.Errorf("Food 5 is not nil")
+	}
+}
