23079047926203aeb6e95ffb4b594eef5464f864
PT GO-JEK INDONESIA <ptgo-jekindonesia@PTs-MacBook-Air-2.local>
2018-07-05 16:34:34 +0700

    add pgsql dump for food database

---
+++ food.sql
@@ -0,0 +1,108 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.4
+-- Dumped by pg_dump version 10.4
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: food; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.food (
+    id integer NOT NULL,
+    name character varying(255) NOT NULL,
+    price integer,
+    owner character varying(255)
+);
+
+
+ALTER TABLE public.food OWNER TO postgres;
+
+--
+-- Name: food_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.food_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.food_id_seq OWNER TO postgres;
+
+--
+-- Name: food_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.food_id_seq OWNED BY public.food.id;
+
+
+--
+-- Name: food id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.food ALTER COLUMN id SET DEFAULT nextval('public.food_id_seq'::regclass);
+
+
+--
+-- Data for Name: food; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.food (id, name, price, owner) FROM stdin;
+1	Nasi Kebuli	38000	Apjay
+2	Chicken Over Rice	55000	Halal Bros
+3	Bakso Gajah	18000	Bakso Gajah
+\.
+
+
+--
+-- Name: food_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.food_id_seq', 3, true);
+
+
+--
+-- Name: food food_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.food
+    ADD CONSTRAINT food_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
